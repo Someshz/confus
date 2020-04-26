21c035ce6a94988bf207be0ceaa1e6e718f152ce
somesh <zadesomesh99@gmail.com>
2020-04-26 17:37:16 +0530

    fetch from server correct

--- src/redux/comments.js
+++ src/redux/comments.js
@@ -5,19 +5,21 @@ export const Comments=(state=
     errmess:null,
     comments:[]},action)=>{
     switch(action.type){
-        case ActionType.ADD_COMMENT:
-            var comment=action.payload;
-            comment.id=state.length;
-            comment.date = new Date().toISOString();
-            console.log("Comment: ", comment);
-            return state.comments.concat(comment)
+        
 
         case ActionType.ADD_COMMENTS:
             return {...state,isLoading:false,errmess:null,comments:action.payload};
     
         case ActionType.COMMENTS_FAILED:
             return {...state,isLoading:false,errmess:action.payload,comments:[]} ;
-        
+
+        case ActionType.ADD_COMMENT:
+            var comment=action.payload;
+            comment.id=state.length;
+            comment.date = new Date().toISOString();
+            console.log("Comment: ", comment);
+            return { ...state, comments: state.comments.concat(comment)};
+                    
         default:
             return state;
 
